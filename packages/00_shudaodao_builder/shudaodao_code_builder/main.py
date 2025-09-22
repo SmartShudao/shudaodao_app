a5f95873375f5832a697d306ff987bb8c4fbfd9a
lf_shao <lf_shao@163.com>
2025-09-22 22:11:39 +0800

    v0.1.1

--- packages/00_shudaodao_builder/shudaodao_code_builder/main.py
+++ packages/00_shudaodao_builder/shudaodao_code_builder/main.py
@@ -10,17 +10,16 @@ if __name__ == "__main__":
     config = GeneratorConfig(
         # 数据库 连接
         db_url="postgresql+psycopg2://postgres:postgres@localhost:5432/postgres",
-        # db_url="sqlite:////Users/lf_shao/GitHub/SmartShudao/shudaodao/database/auth_user.db",
         # 鉴权用的角色(默认 schema_name)
         output_auth_role_name=None,
         # 实体模型: 是否输出 schema 标签
         output_schema_to_entity=True,
-        # 路由路径: 一般不用设置，默认用 schema，如果数据库不支持 schema 建议设置
+        # router_path : 一般不用设置，默认用 schema，如果数据库不支持 schema 建议设置
         # output_router_path="shudaodao_aaa",
-        # 路由模型: 获取数据源    来自 xxx.yaml - storage - database - name: Core Common Generate 定义
+        # 数据库连接名称    来自 xxx.yaml - storage - database - name: Core Common Generate 定义
         output_db_engine_name="Generate",
         # 输出目录: 默认: {project_root}/src/shudaodao_generate
-        output_path=None
+        # output_path=None
     )
     # --------------------------------------------
     #           生成器 用法
