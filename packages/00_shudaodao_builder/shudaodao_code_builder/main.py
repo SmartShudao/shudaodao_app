539a68cc990ab9520a60bbdb46e92d0d0599e217
lf_shao <lf_shao@163.com>
2025-09-26 01:13:26 +0800

    v0.1.1

--- packages/00_shudaodao_builder/shudaodao_code_builder/main.py
+++ packages/00_shudaodao_builder/shudaodao_code_builder/main.py
@@ -10,14 +10,13 @@ if __name__ == "__main__":
     config = GeneratorConfig(
         # 数据库 连接
         db_url="postgresql+psycopg2://postgres:postgres@localhost:5432/postgres",
-        # 鉴权用的角色(默认 schema_name)
-        output_auth_role_name=None,
-        # 实体模型: 是否输出 schema 标签
-        output_schema_to_entity=True,
-        # router_path : 一般不用设置，默认用 schema，如果数据库不支持 schema 建议设置
-        # output_router_path="shudaodao_aaa",
+
+        # 如果数据库不支持 schema 时，设置default_schema
+        # default_schema_name="shudaodao_aaa",
+
         # 数据库连接名称    来自 xxx.yaml - storage - database - name: Core Common Generate 定义
-        output_db_engine_name="Generate",
+        database_engine_name="Generate",
+
         # 输出目录: 默认: {project_root}/src/shudaodao_generate
         # output_path=None
     )
@@ -30,7 +29,8 @@ if __name__ == "__main__":
     builder = GeneratorService(config)
     # builder.generator_table(table_name="number", schema_name="shudao_acm")
     # builder.generator_view(view_name="v_donelist", schema_name="shudao_bpm")
-    builder.generator_schema(schema_name="shudao_acm")
-    builder.generator_schema(schema_name="shudao_bpm")
-    builder.generator_schema(schema_name="shudao_admin")
-    # builder.generator_all()
+    # builder.generator_schema(schema_name="shudao_acm")
+    # builder.generator_schema(schema_name="shudao_admin")
+    # builder.generator_schema(schema_name="shudao_bpm")
+    # builder.generator_schema(schema_name="shudaodao_core")
+    builder.generator_all()
